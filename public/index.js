370f51924c0f57872013972083368b03970345e1
Nick <nicholas.m.ricci@gmail.com>
2024-02-05 22:12:19 +0000

    A2

---
+++ public/index.js
@@ -0,0 +1,34 @@
+function setNavigationActive() {
+    let elems = [...document.querySelectorAll(".wd-kanbas-navigation > li")]
+    let id = window.location.pathname.split("/")[2];
+    elems.forEach((elem) => {
+        if (elem.id == id) {
+            elem.className = "wd-active"
+        }
+    })
+}
+
+async function setTriFold(nodes) {
+    let elems = [...nodes]
+    let promises = []
+
+    for (const elem of elems) {
+        promises.push(fetch(elem.id).then((resp) => {
+            return resp.text();
+        }).then((html) => {
+            elem.innerHTML = html;
+        }))
+    }
+
+    await Promise.all(promises)
+}
+
+async function setCourseNavigationActive() {
+    let elems = [...document.querySelectorAll(".wd-navigation > li")]
+    let id = window.location.pathname.split("/")[3];
+    elems.forEach((elem) => {
+        if (elem.id == id) {
+            elem.className = "wd-active"
+        }
+    })
+}
